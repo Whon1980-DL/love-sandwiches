af2811b0954da36ed05edd1416708a96f7eeee5f
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2024-11-25 08:57:50 +0000

    Add function to calculate how much sadwich to make for next market

--- run.py
+++ run.py
@@ -128,6 +128,16 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+
+    print("Make the following numbers of sandwiches for next market:\n")
+    
+    return {heading: data for heading, data in zip(headings, data)}   
+
 def main():
     """
     Run all program function
@@ -140,8 +150,15 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
+
+
+    
+
+
 
 
